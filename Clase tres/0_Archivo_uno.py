1875a1faf4ae5a96cdc66603a4c65e042250a853
AndresMira21 <130936563+nicodelgaaado@users.noreply.github.com>
2024-04-27 19:01:29 -0500

    Clase 3
    Se creo un archivo con tipos de variables de la lista

---
+++ Clase tres/0_Archivo_uno.py
@@ -0,0 +1,25 @@
+#esta es una variable
+variable = 8
+variable_dos = "texto dos"
+my_string = "thisStringAwesone"
+print(variable_dos)
+print(my_string)
+
+my_string.upper()
+print(my_string.upper())
+#print () permite mostrar en la terminal la informacion
+""""
+print("Hola")
+print(type(8))
+print(type(1.8))
+print(type("texto"))
+"""
+
+
+
+
+
+
+
+
+
